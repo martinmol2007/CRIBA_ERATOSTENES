3cddbf508dba6248cd3d34e3be0a84914da41219
Martin <martinmolinapavon1970@gmail.com>
2026-08-11 17:53:11 +0200

    Cambios

--- main.cc
+++ main.cc
@@ -4,7 +4,91 @@
 
 using namespace std;
 
+
+/**
+ * @brief Rellena el vector con todos los numeros consecutivos empezando por el 0 hasta el numero "num"
+ * 
+ * @param v Vector a rellenar
+ * @param num Numero maximo
+ */
+void rellenar_vector (vector<int>& v, int num) {
+    for(int i = 0; i < num+1; i++) {
+        v[i] = i;
+    }
+
+    // El 0 y el 1 no son primos
+    v[0] = 0;
+    v[1] = 0;
+
+    return;
+}
+
+
+/**
+ * @brief Consigue los numeros primos dado un vector
+ * 
+ * @param v Vector con todos los numeros consecutivos
+ * @param num Numero limite
+ * @return vector<int> Vector resultante
+ */
+vector<int> conseguir_primos (vector<int>& v, int num) {
+    // Crea el vector resultado
+    vector<int> res;
+
+    // Comprueba si son primos
+    for(int i = 2; i*i <= num; i++) {
+        // Si lo es (no es 0), añadelo al vector resultado
+        if(v[i] != 0) {
+            res.push_back(v[i]);
+            // Pon a 0 todos sus multiples
+            for(int j = i * i; j <= num; j += i) {
+                v[j] = 0;
+            }
+        }
+    }
+
+    return res;
+}
+
 int main() {
-   
+
+    // Menu inicial
+
+    cout << "Bienvenido a la Criba de Eratostenes!" << endl;
+    cout << "Introduce un numero aleatorio (mas grande que 1), y veremos cuantos primos hay: ";
+
+    // Consigue el numero para saber cuantos primos hay
+
+    bool correcto = false;
+    int num;
+
+    while(not correcto) {
+        cin >> num;
+        if(num > 1) {
+            correcto = true;
+        } else {
+            cout << "Error, introduce un numero aleatorio (mas grande que 1), y veremos cuantos primos hay: ";
+        }
+    }
+
+    // Vector de tamaña del numero indicado y asigna todos los numeros posibles
+    vector<int> vec(num);
+    rellenar_vector(vec, num);
+
+    // Consigue los primos
+    vector<int> res = conseguir_primos(vec, num);
+
+    // Menu de salida
+    cout << endl;
+ 
+    cout << "Los numeros primos son: " << endl;
+
+    for(int i = 0; i < res.size(); i++) {
+        if(i > 0) cout << " ";
+        cout << res[i];
+    }
+
+    cout << endl;
+
     return 0;
 }
